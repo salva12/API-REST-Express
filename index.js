bfce2efedfc5fdd4dde70df4290faebe8a9a2ff4
salva <salvamanu12@gmail.com>
2023-03-01 22:49:11 -0300

    feat: :zap: Add CRUD Rest methods

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const routerApi = require('./routes');
 const app = express();
 const port = 3000;
 
+app.use(express.json());
 
 routerApi(app);
 
--- routes/products.router.js
+++ routes/products.router.js
@@ -38,4 +38,30 @@ router.get('/:id', (req,res)=>{
     });
 });
 
+router.post('/',(req,res)=>{
+  const body = req.body;
+  res.json({
+    messge:'created',
+    data:body
+  });
+})
+
+router.patch('/:id',(req,res)=>{
+  const {id} = req.params;
+  const body = req.body;
+  res.json({
+    message:'update',
+    data:body,
+    id
+  });
+})
+
+router.delete('/:id',(req,res)=>{
+  const {id} = req.params;
+  res.json({
+    message:'deleted',
+    id
+  });
+})
+
 module.exports = router;
