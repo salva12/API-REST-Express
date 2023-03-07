bf78fc7320b2b076d92e2132e7ac76080b9d5b3b
salva <salvamanu12@gmail.com>
2023-03-06 21:05:53 -0300

    feat: :art: adding CRUD methods service

--- services/product.service.js
+++ services/product.service.js
@@ -35,8 +35,16 @@ class ProductsService {
     ];
   }
 
-  create(){
-
+  create(data){
+    let ids = [];
+    this.products.map(item =>ids.push(item.id));
+    let idMax = Math.max(...ids);
+    const newProduct = {
+      id:idMax+1,
+      ...data
+    }
+    this.products.push(newProduct);
+    return newProduct;
   }
 
   find(){
@@ -47,12 +55,25 @@ class ProductsService {
     return this.products.find(item =>item.id == id);
   }
 
-  update(){
-
+  update(id,changes){
+    const index = this.products.findIndex(item =>item.id==id);
+    if(index===-1){
+      throw new Error('product not found')
+    }
+    this.products[index]={
+      ...this.products[index],
+      ...changes
+    };
+    return this.products[index];
   }
 
-  delete(){
-
+  delete(id){
+    const index = this.products.findIndex(item =>item.id==id);
+    if(index===-1){
+      throw new Error('product not found')
+    }
+    this.products.splice(index,1);
+    return {id};
   }
 
 }
